b771e4278a7f5ef6b078390cd3b72c77348762b2
qinyi2922 <55088771+qinyi2922@users.noreply.github.com>
2019-09-16 16:29:32 +0800

    搜索、替换

--- mainwindow.h
+++ mainwindow.h
@@ -7,27 +7,18 @@
 #include<QMenuBar>
 #include<QAction>
 #include<QProcess>
-#include <QFileDialog>
-#include <QTextStream>
-#include <QMessageBox>
-#include <QDialog>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QMainWindow>
-#include<QTextEdit>
 #include <QWidget>
 #include <QDialog>
 #include <QGridLayout>
+#include <QHBoxLayout>
 #include <QLabel>
+#include <QLineEdit>
 #include <QGroupBox>
+#include <QPushButton>
 #include <QCheckBox>
 #include <QRadioButton>
 #include <QEvent>
 #include "qinputdialog.h"
-class QDialog;
-class QLineEdit;
 namespace Ui {
 class MainWindow;
 }
@@ -39,42 +30,52 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-    QString currentName;
-    bool maybeSave();
-    void setCurrentFile(const QString &);
-    bool saveFile(const QString &);
-    void loadFile(const QString &);
-    bool compile(const QString &);
-    bool compile2(const QString &);
+//替换
+    QLabel *searchLabel;
+
+    QLabel *replaceLabel;
+
+    QLineEdit *searchLineEdit;
+
+    QLineEdit *replaceLineEdit;
+
+    QPushButton *sureBtn;
+//搜索
+    QLabel *searchInputLabel;
+    QPushButton *searchForwardBtn;
+    QLineEdit *searchInputLineEdit;
+
+    QDialog *searchInputDialog;
+    QDialog *replaceDialog;
+
 private:
     QString filename;
     QString savefilename;
     QProcess *process;
 private slots:
-     void show_findText();
-     void on_copy();
-     void on_paste();
-     void on_cut();
-     void on_about();
-     void on_exit();
-     void on_selectall();
-     void on_redo();
-     void on_undo();
-     void on_delete();
-     void on_open();
-     bool on_compile();
-     bool on_save();
-     bool on_saveas();
-     bool on_run();
-     void on_new();
+     //void on_copy();
+     //void on_paste();
+     //void on_cut();
+     //void on_about();
+     //void on_exit();
+     //void on_selectall();
+     //void on_redo();
+     //void on_undo();
+     //void on_delete();
+     //void on_open();
+     //void on_compile();
+     //void on_save();
+     //void on_run();
+     //void on_new();
      void on_search();
      void on_replace();
-     void on_character();
      void searchString();
      void replaceString();
+//   void on_character();
 //   void on_environment();
+
 private:
     Ui::MainWindow *ui;
-    QLineEdit *find_textLineEdit;
+
 };
 #endif //MAINWINDOW_H
